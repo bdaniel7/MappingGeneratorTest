0bcbd258ea29c732311926730c5aee024152c238
Daniel Blendea <bdaniel7@gmail.com>
2020-10-14 17:40:49 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MappingGeneratorTest
+{
+	public class Class1
+	{
+		public string TempFlagRd { get; set; }
+		public int? DocumentLegacyId { get; set; }
+		public int? RegulationLegacyId { get; set; }
+		public int? CountryId { get; set; }
+		public string TitleShort { get; set; }
+		public string OfficialTitle { get; set; }
+
+		public Class1 Clone()
+		{
+			return new Class1
+			       {
+								
+			       };
+		}
+	}
+}
